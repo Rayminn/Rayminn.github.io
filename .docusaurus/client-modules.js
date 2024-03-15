1378282724cb8577ce959f549a212bbb371b1cca
Rayminn <98998872+Rayminn@users.noreply.github.com>
2024-03-15 07:51:34 +0000

    upd ign

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/workspaces/Rayminn.github.io/node_modules/infima/dist/css/default/default.css'),
-  require('/workspaces/Rayminn.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/workspaces/Rayminn.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/workspaces/Rayminn.github.io/src/css/custom.scss'),
-  require('/workspaces/Rayminn.github.io/node_modules/docusaurus-plugin-image-zoom/src/zoom'),
-];
